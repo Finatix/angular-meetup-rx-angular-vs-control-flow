580de8fb68e7c1f6db29f634217622ae9ffec15d
Christian Illies (FINATIX) <c.illies@finatix.de>
2023-12-04 20:18:42 +0100

    test setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { screen, render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'angular-meetup-rx-angular-gegen-control-flow' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-meetup-rx-angular-gegen-control-flow');
+    await render(AppComponent);
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to angular-meetup-rx-angular-gegen-control-flow!');
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent('Welcome to angular-meetup-rx-angular-gegen-control-flow!');
   });
 });
