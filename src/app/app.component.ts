455b3c815e92c1faa4bfe1a51529021be2182a46
Christian Illies (FINATIX) <c.illies@finatix.de>
2024-01-08 13:59:40 +0100

    add scheduler sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import {RxStrategyProvider} from "@rx-angular/cdk/render-strategies";
 import {mySlowFunction} from "./my-slow.function";
 
+const now = () => Math.floor(performance.now());
+
 @Component({
   selector: 'mee-root',
   standalone: true,
@@ -30,7 +32,7 @@ import {mySlowFunction} from "./my-slow.function";
     <footer>
     <section>
       <p>More Rendering</p>
-      <img [ngSrc]="'assets/martin-adams-pTCcJSBOTxY-unsplash.jpg'" width="10633" height="4000" alt="">
+      <img [ngSrc]="'assets/martin-adams-pTCcJSBOTxY-unsplash.jpg'" priority="" width="10633" height="4000" alt="">
     </section>
     </footer>
 
@@ -45,39 +47,49 @@ export class AppComponent {
   constructor() {
     requestIdleCallback(() => {
       const result = mySlowFunction();
-      console.log(performance.now(), this.constructor.name, 'idle native/result', result)
+      console.log(now(), this.constructor.name, 'idle native', result)
     });
     setTimeout(() => {
       const result = mySlowFunction();
-      console.log(performance.now(), this.constructor.name, 'setTimeout native/result', result)
+      console.log(now(), this.constructor.name, 'setTimeout native', result)
     });
     Promise.resolve().then(() => {
       const result = mySlowFunction();
-      console.log(performance.now(), this.constructor.name, 'Promise native/result', result)
+      console.log(now(), this.constructor.name, 'Promise native', result)
     });
     queueMicrotask(() => {
       const result = mySlowFunction();
-      console.log(performance.now(), this.constructor.name, 'queueMicrotask native/result', result)
+      console.log(now(), this.constructor.name, 'queueMicrotask native', result)
     });
+
+    if ("scheduler" in window) {
+      // three tasks, in reverse order of priority
+      // @ts-ignore
+      scheduler.postTask(() => mySlowFunction(), { priority: "background" }).then((result: number) => console.log(now(), this.constructor.name, "postTask/background", result));
+      // @ts-ignore
+      scheduler.postTask(() => mySlowFunction(), { priority: "user-visible" }).then((result: number) => console.log(now(), this.constructor.name, "postTask/user-visible", result));
+      // @ts-ignore
+      scheduler.postTask(() => mySlowFunction(), { priority: "user-blocking" }).then((result: number) => console.log(now(), this.constructor.name, "postTask/user-blocking", result));
+    }
 
     this.strategyProvider
       .schedule(() => mySlowFunction(), { strategy: 'idle' })
-      .subscribe(result => console.log(performance.now(), this.constructor.name, 'idle rx/result', result));
+      .subscribe(result => console.log(now(), this.constructor.name, 'idle rx-angular', result));
 
     this.strategyProvider
       .schedule(() => mySlowFunction(), { strategy: 'low' })
-      .subscribe(result => console.log(performance.now(), this.constructor.name, 'low rx/result', result));
+      .subscribe(result => console.log(now(), this.constructor.name, 'low rx-angular', result));
 
     this.strategyProvider
       .schedule(() => mySlowFunction(), { strategy: 'normal' })
-      .subscribe(result => console.log(performance.now(), this.constructor.name, 'normal rx/result', result));
+      .subscribe(result => console.log(now(), this.constructor.name, 'normal rx-angular', result));
 
     this.strategyProvider
       .schedule(() => mySlowFunction(), { strategy: 'userBlocking' })
-      .subscribe(result => console.log(performance.now(), this.constructor.name, 'userBlocking rx/result', result));
+      .subscribe(result => console.log(now(), this.constructor.name, 'userBlocking rx-angular', result));
 
     this.strategyProvider
       .schedule(() => mySlowFunction(), { strategy: 'immediate' })
-      .subscribe(result => console.log(performance.now(), this.constructor.name, 'immediate rx/result', result));
+      .subscribe(result => console.log(now(), this.constructor.name, 'immediate rx-angular', result));
   }
 }
